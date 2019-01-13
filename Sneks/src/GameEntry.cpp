d909f5b2eefe8774a07e95c25812f0a632e06c6a
Javier <javier_foo@hotmail.com>
2019-01-14 01:17:43 +0800

    New bodies and head

--- Sneks/src/GameEntry.cpp
+++ Sneks/src/GameEntry.cpp
@@ -22,18 +22,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	AESysReset();
 	AEGfxSetBackgroundColor(1, 1, 0);
 
-	auto snakeHeadTexture  = AEGfxTextureLoad("../Resources/snake-head.png");
+	auto snakeHeadTexture  = AEGfxTextureLoad("../Resources/head.png");
 	auto snakeHeadLTexture = AEGfxTextureLoad("../Resources/snek_hed_l.jpg");
 	auto snakeHeadRTexture = AEGfxTextureLoad("../Resources/snek_hed_r.jpg");
-	auto snakeBodyTexture  = AEGfxTextureLoad("../Resources/snake-body.png");
+	auto snakeBodyTexture  = AEGfxTextureLoad("../Resources/body.png");
 	auto rocketTexture     = AEGfxTextureLoad("../Resources/rocket_booster.jpg");
 	auto smokeTexture      = AEGfxTextureLoad("../Resources/smoke.jpg");
 
-	auto snekHeadTest = static_cast<SnekHead*>(new SnekHead(500, 0, 105, 77, snakeHeadTexture));
+	auto snekHeadTest = static_cast<SnekHead*>(new SnekHead(500, 0, 108, 78, snakeHeadTexture));
 	snekHeadTest->SetParticles(smokeTexture, rocketTexture);
 	snekHeadTest->SetColor(9999);
 
-	auto snekHeadTest2 = static_cast<SnekHead*>(new SnekHead(-150, 0, 105, 77, snakeHeadTexture));
+	auto snekHeadTest2 = static_cast<SnekHead*>(new SnekHead(-150, 0, 108, 78, snakeHeadTexture));
 	snekHeadTest2->SetParticles(smokeTexture, rocketTexture);
 	srand(time(nullptr));
 
@@ -43,9 +43,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	auto snek2 = static_cast<Snek*>(new Snek(snekHeadTest2));
 	snek2->SetPlayer(1);
 	for (int iBodyParts = 0; iBodyParts < kNumBodyParts; iBodyParts++) {
-		auto snekBodyTest = static_cast<SnekBody*>(new DrawObject(100, 0, 61, 80, snakeBodyTexture));
+		auto snekBodyTest = static_cast<SnekBody*>(new DrawObject(100, 0, 61, 84, snakeBodyTexture));
 		snek->AddBodyPart(snekBodyTest);
-		auto snekBodyTest2 = static_cast<SnekBody*>(new DrawObject(100, 0, 61, 80, snakeBodyTexture));
+		auto snekBodyTest2 = static_cast<SnekBody*>(new DrawObject(100, 0, 61, 84, snakeBodyTexture));
 		snekBodyTest2->SetColor(rand() % 10000);
 		snek2->AddBodyPart(snekBodyTest2);
 	}
