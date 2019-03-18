9acb408691528b59ba66ebba4783b4e304e1d15d
Lim Chu Yan <findcylim@gmail.com>
2019-03-19 03:28:50 +0800

    Reduced bloom

--- Sneks/src/Components/BloomComponent.h
+++ Sneks/src/Components/BloomComponent.h
@@ -8,7 +8,7 @@
 class BloomComponent : public BaseComponent
 {
 public:
-	float m_f_BloomStrength = 0.33f; // 0.33f default alpha of drawn blurs
+	float m_f_BloomStrength = 0.2f; // 0.33f default alpha of drawn blurs
 	float m_f_BloomDiffuse = 2.0f;   // pixel offset of drawn blurs
 	int   m_i_BloomIterations = 2;   // number of drawn blurs
 };
