ec1bf7aa926c6f85e0c250f1e3685314c6102c0f
m.muhammadadambin <m.muhammadadambin@DIT0341SG>
2019-04-02 12:54:40 +0800

    Fixed the credits offset changes.

--- Sneks/src/Systems/Menus/CreditsScreenSystem.cpp
+++ Sneks/src/Systems/Menus/CreditsScreenSystem.cpp
@@ -20,9 +20,9 @@ void CreditsScreenSystem::Initialize()
 													   kCanvasBasicSprite,"CreditPortrait" ,"CY" ,"","","", nullptr };
 	
 
-	Events::EV_NEW_UI_ELEMENT MemberNameUIElement = { canvas_Component, HTVector2{ 0.35f , 0.5f } ,
+	Events::EV_NEW_UI_ELEMENT MemberNameUIElement = { canvas_Component, HTVector2{ 0.37f , 0.5f } ,
 														 kCanvasTextLabel,"NameTextLabelEntity" ,"" ,"Lim Chu Yan","","", nullptr ,HTColor{1.0f,1.0f,1.0f,0.0f} };
-	Events::EV_NEW_UI_ELEMENT MemberRoleUIElement = { canvas_Component, HTVector2{ 0.35f , 0.45f } ,
+	Events::EV_NEW_UI_ELEMENT MemberRoleUIElement = { canvas_Component, HTVector2{ 0.37f , 0.45f } ,
 														 kCanvasTextLabel,"RoleTextLabelEntity" ,"" ,"Tech Lead","","", nullptr,HTColor{1.0f,1.0f,1.0f,0.0f} };
 
 	Events::EV_NEW_UI_ELEMENT TransitonBackUIElement = { canvas_Component, HTVector2{ 0.5f , 0.5f } ,
@@ -50,6 +50,15 @@ void CreditsScreenSystem::Receive(const Events::EV_MOUSE_ONCLICK& eventData)
 		GameStateManager::SetState(m_e_PrevState);
 }
 
+void CreditsScreenSystem::UpdateText(CanvasTextLabelEntity* textEntity,const char * nameText,HTVector2 ScreenOffset)
+{
+	auto NameTextRenderer = textEntity->GetComponent<TextRendererComponent>();
+	auto NameUIComponent = textEntity->GetComponent<CanvasElementComponent>();
+	NameTextRenderer->CreateText(NameUIComponent->m_f_XOffset + static_cast<float>(strlen(nameText) * -7.5f),
+		ScreenOffset.y - NameUIComponent->m_f_YOffset + -13.5f, nameText, HTColor{ 1,1,1,1 });
+
+}
+
 void CreditsScreenSystem::Update(float dt)
 {
 	m_f_Timer -= dt;
@@ -68,27 +77,36 @@ void CreditsScreenSystem::Update(float dt)
 		if (m_i_PortraitValue > 3)
 			m_i_PortraitValue = 0;
 
+
+		HTVector2 ScreenSize;
+		AlphaEngineHelper::GetScreenSize(&ScreenSize.x, &ScreenSize.y);
+
+
 		switch (m_i_PortraitValue)
 		{
 		case 0:
 			PortraitEntity->GetComponent<DrawComponent>()->m_px_Texture = m_po_GraphicsSystem->FetchTexture("Spoodermun");
-			NameEntity->GetComponent<TextRendererComponent>()->SetText("Javier Foo");
-			RoleEntity->GetComponent<TextRendererComponent>()->SetText("Lead Designer");
+			
+			UpdateText(NameEntity, "Javier Foo", ScreenSize);
+			UpdateText(RoleEntity, "Lead Designer", ScreenSize);
 			break;
 		case 1:
 			PortraitEntity->GetComponent<DrawComponent>()->m_px_Texture = m_po_GraphicsSystem->FetchTexture("CY");
-			NameEntity->GetComponent<TextRendererComponent>()->SetText("Lim Chu Yan");
-			RoleEntity->GetComponent<TextRendererComponent>()->SetText("Tech Lead");
+
+			UpdateText(NameEntity, "Lim Chu Yan", ScreenSize);
+			UpdateText(RoleEntity, "Tech Lead", ScreenSize);
 			break;
 		case 2:
 			PortraitEntity->GetComponent<DrawComponent>()->m_px_Texture = m_po_GraphicsSystem->FetchTexture("Edwin");
-			NameEntity->GetComponent<TextRendererComponent>()->SetText("Edwin Khoo Bing Shen");
-			RoleEntity->GetComponent<TextRendererComponent>()->SetText("Product Manager");
+
+			UpdateText(NameEntity, "Edwin Khoo", ScreenSize);
+			UpdateText(RoleEntity, "Product Manager", ScreenSize);
 			break;
 		case 3:
 			PortraitEntity->GetComponent<DrawComponent>()->m_px_Texture = m_po_GraphicsSystem->FetchTexture("Adam");
-			NameEntity->GetComponent<TextRendererComponent>()->SetText("Muhammad Adam Bin Mohamad Amin");
-			RoleEntity->GetComponent<TextRendererComponent>()->SetText("Producer");
+
+			UpdateText(NameEntity, "Adam Amin", ScreenSize);
+			UpdateText(RoleEntity, "Producer", ScreenSize);
 			break;
 		}
 
